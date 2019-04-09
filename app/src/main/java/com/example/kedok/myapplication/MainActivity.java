7c33cea2463c8565c1396f7dd417ee63bdb886e3
Kedok <zaenudinm844@gmail.com>
2019-04-09 19:49:58 +0700

    commitTest

--- app/src/main/java/com/example/kedok/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kedok/myapplication/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText emiailku, passku;
+    EditText emailku, passku;
     Button login;
 
     @Override
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        emiailku = (EditText) findViewById(R.id.email);
+        emailku = (EditText) findViewById(R.id.email);
         passku = (EditText) findViewById(R.id.pass);
         login = (Button) findViewById(R.id.btn_login);
 
@@ -28,8 +28,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent move = new Intent(MainActivity.this,Move.class);
-                startActivity(move);
+                String email_key = emailku.getText().toString();
+                String pass_key = passku.getText().toString();
+
+                if (email_key.equals("latihan_modul_3") && pass_key.equals("123masuk")){
+
+                    Intent move = new Intent(MainActivity.this,Move.class);
+                    startActivity(move);
+                }else {
+                    AlertDialog.Builder builder =  new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Email & Password Anda Salah").setNegativeButton("coba lagi",null).create().show();
+
+                }
+
 
             }
         });
