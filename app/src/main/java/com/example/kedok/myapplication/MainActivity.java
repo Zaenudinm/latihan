7428cecdb2b60b17bc2ca34ce5110d006aeeceaa
Kedok <zaenudinm844@gmail.com>
2019-04-15 16:16:25 +0700

    commitTest

--- app/src/main/java/com/example/kedok/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kedok/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText emailku, passku;
     Button login;
+    String TAG = "Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +31,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 String email_key = emailku.getText().toString();
+                Log.d(TAG, email_key);
+
                 String pass_key = passku.getText().toString();
+                Log.d(TAG, pass_key);
 
                 if (email_key.equals("latihan_modul_3") && pass_key.equals("123masuk")){
-
                     Intent move = new Intent(MainActivity.this,Move.class);
+                    move.putExtra("Nama Email Ku", email_key);
+                    move.putExtra("Password Email Ku", pass_key);
                     startActivity(move);
                 }else {
                     AlertDialog.Builder builder =  new AlertDialog.Builder(MainActivity.this);
--- app/src/main/java/com/example/kedok/myapplication/Move.java
+++ app/src/main/java/com/example/kedok/myapplication/Move.java
@@ -2,6 +2,7 @@ package com.example.kedok.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Move extends AppCompatActivity {
 
@@ -9,6 +10,14 @@ public class Move extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_move);
+
+        TextView nameEmail = findViewById(R.id.txt_emailku);
+        String EmailName = getIntent().getStringExtra("Nama Email Ku");
+        nameEmail.setText(EmailName);
+
+        TextView passEmail = findViewById(R.id.txt_passku);
+        String PassEmailku = getIntent().getStringExtra("Password Email Ku");
+        passEmail.setText(PassEmailku);
 
     }
 }
